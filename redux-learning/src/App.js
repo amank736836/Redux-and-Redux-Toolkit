e6a17354642417f853641ef739ed5ea69f5ac861
Aman Kumar <amankarguwal0@gmail.com>
2024-07-07 15:21:25 +0530

    refactor: Update App.js to use useSelector hook for custom state

--- redux-learning/src/App.js
+++ redux-learning/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from "react-redux";
+import "./App.css";
+import Home from "./Home.jsx";
 
 function App() {
+  const { c } = useSelector((state) => state.custom);
+
+  // const value = 30;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{c}</h1>
+
+      {/* {c} */}
+      <Home
+      //  value1={value}
+      />
     </div>
   );
 }
